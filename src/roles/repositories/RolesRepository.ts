b5f30c68b8b4a21f993062b133103bc8c48414e8
AugustoTI <augustocesarti2021@gmail.com>
2022-11-02 18:37:29 -0300

    add roles repository

---
+++ src/roles/repositories/RolesRepository.ts
@@ -0,0 +1,19 @@
+import { Role } from '@roles/entities/role'
+
+interface CreateRoleDTO {
+  name: string
+}
+
+export class RoleRepository {
+  private _roles: Role[] = []
+
+  create({ name }: CreateRoleDTO) {
+    const role = new Role()
+
+    Object.assign(role, { name, created_at: new Date() })
+
+    this._roles.push(role)
+
+    return role
+  }
+}
