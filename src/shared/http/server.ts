0b4942cc15080916eece770da32a4e52a486b18e
AugustoTI <augustocesarti2021@gmail.com>
2022-11-01 22:15:41 -0300

    add errors middleware

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
 import 'express-async-errors'
 import { routes } from './routes'
+import { AppError } from '@shared/errors/AppError'
 
 const server = express()
 
@@ -10,6 +11,17 @@ server.use(cors())
 server.use(express.json())
 
 server.use(routes)
+server.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    res.status(error.status).json({ status: 'error', message: error.message })
+  }
+
+  console.log(error)
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal error server',
+  })
+})
 
 server.listen(process.env.PORT, () => {
   console.log(`http://localhost:${process.env.PORT}`)
