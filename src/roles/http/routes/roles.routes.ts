ca038a62288eca546b6646dbd55142db4f1c332a
AugustoTI <augustocesarti2021@gmail.com>
2022-11-02 00:04:48 -0300

    add roles routes

---
+++ src/roles/http/routes/roles.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express'
+import { v4 as uuidV4 } from 'uuid'
+
+const rolesRouter = Router()
+
+const roles = []
+
+rolesRouter.post('/', (req, res) => {
+  const { name } = req.body
+  const role = {
+    id: uuidV4(),
+    name,
+    created_at: new Date(),
+  }
+
+  roles.push(role)
+
+  return res.status(201).json({ role })
+})
+
+export { rolesRouter }
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,11 +1,12 @@
+import { rolesRouter } from '@roles/http/routes/roles.routes'
 import { Router } from 'express'
 
 const routes = Router()
 
 routes.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello Devs',
-  })
+  return res.json({ message: 'Hello Devs' })
 })
+
+routes.use('/roles', rolesRouter)
 
 export { routes }
