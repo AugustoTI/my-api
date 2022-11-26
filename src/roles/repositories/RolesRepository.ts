7b94fa0495a28f6e007d496a21a10f2625c5c1a5
AugustoTI <augustocesarti2021@gmail.com>
2022-11-26 12:09:44 -0300

    refactor role repository

--- src/roles/repositories/RolesRepository.ts
+++ src/roles/repositories/RolesRepository.ts
@@ -1,16 +1,31 @@
 import { Role } from '@roles/entities/role'
+import { dataSource } from '@shared/typeorm'
+import { Repository } from 'typeorm'
 
 // DTO = Data Transfer Object
 interface CreateRoleDTO {
   name: string
 }
 
+export interface PaginateParams {
+  page: number
+  skip: number
+  take: number
+}
+
+export interface RolesPaginateProperties {
+  per_page: number
+  total: number
+  current_page: number
+  data: Role[]
+}
+
 export class RoleRepository {
-  private _roles: Role[]
+  private _repository: Repository<Role>
   private static INSTANCE: RoleRepository
 
   private constructor() {
-    this._roles = []
+    this._repository = dataSource.getRepository(Role)
   }
 
   static getInstance() {
@@ -22,21 +37,41 @@ export class RoleRepository {
     return RoleRepository.INSTANCE
   }
 
-  create({ name }: CreateRoleDTO) {
-    const role = new Role()
-
-    Object.assign(role, { name, created_at: new Date() })
-
-    this._roles.push(role)
-
-    return role
+  async create({ name }: CreateRoleDTO) {
+    const role = this._repository.create({ name })
+    return this._repository.save(role)
   }
 
-  findByName(name: string) {
-    return this._roles.find(role => role.name === name)
+  async save(role: Role) {
+    return this._repository.save(role)
   }
 
-  findAll() {
-    return this._roles
+  async delete(role: Role) {
+    await this._repository.remove(role)
+  }
+
+  async findByName(name: string) {
+    return this._repository.findOneBy({ name })
+  }
+
+  async findById(id: string) {
+    return this._repository.findOneBy({ id })
+  }
+
+  async findAll({ page, skip, take }: PaginateParams) {
+    const [roles, count] = await this._repository
+      .createQueryBuilder()
+      .skip(skip)
+      .take(take)
+      .getManyAndCount()
+
+    const result: RolesPaginateProperties = {
+      current_page: page,
+      per_page: take,
+      data: roles,
+      total: count,
+    }
+
+    return result
   }
 }
