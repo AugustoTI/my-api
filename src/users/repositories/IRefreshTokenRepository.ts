cefe30043cadaa4c651eda0115aa51e21246260c
AugustoTI <augustocesarti2021@gmail.com>
2022-12-06 19:50:05 -0300

    create RefreshToken interface

---
+++ src/users/repositories/IRefreshTokenRepository.ts
@@ -0,0 +1,19 @@
+import { RefreshToken } from '@users/entities/RefreshToken'
+
+interface CreateRefreshTokenDTO {
+  token: string
+  valid: boolean
+  user_id: string
+  expires: Date
+}
+
+export interface IRefreshTokenRepository {
+  create({
+    expires,
+    token,
+    user_id,
+    valid,
+  }: CreateRefreshTokenDTO): Promise<RefreshToken>
+  findByToken(token: string): Promise<RefreshToken | null>
+  invalidate(refresh_token: RefreshToken): Promise<void>
+}
